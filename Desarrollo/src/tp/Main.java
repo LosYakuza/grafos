ef8bb4cc481bc1929373a109933bc8756ee0611d
Juan Arana <arana.juanpablo@gmail.com>
2017-06-22 21:35:46 -0300

    Divididas ejecuciones en particiones

--- Desarrollo/src/tp/Main.java
+++ Desarrollo/src/tp/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import grafos.GrafoGenerator;
+import grafos.GrafoNDNP;
 
 
 public class Main {
@@ -18,11 +19,29 @@ public class Main {
 		int processors = Runtime.getRuntime().availableProcessors();
 		processors = processors -1; // dejame usar la pc un toque
 		System.out.println("Procesadores disponibles: "+processors);
-		tareas.add( new MulticoreEject(GrafoGenerator.porcAdyacencia(600, 60), "60060"));
-		tareas.add( new MulticoreEject(GrafoGenerator.porcAdyacencia(600, 90), "60090"));
-		tareas.add( new MulticoreEject(GrafoGenerator.porcAdyacencia(600, 40), "60040"));
-		tareas.add( new MulticoreEject(GrafoGenerator.regularPorcAdyacencia(1000, 50), "R100050"));
-		tareas.add( new MulticoreEject(GrafoGenerator.regularPorcAdyacencia(1000, 75), "R100075"));
+		
+		tareas.add( new MulticoreEject(GrafoGenerator.porcAdyacencia(600, 40), "60040",CORRIDAS));
+		
+		tareas.add( new MulticoreEject(GrafoGenerator.porcAdyacencia(600, 60), "60060",CORRIDAS));
+		
+		
+		/*divido en mas procesos tareas largas*/
+		GrafoNDNP g = GrafoGenerator.porcAdyacencia(600, 90);
+		g.setGrados();
+		tareas.add( new MulticoreEject(g, "60090-0",CORRIDAS/2));
+		tareas.add( new MulticoreEject(g, "60090-1",CORRIDAS/2));
+		
+		g = GrafoGenerator.regularPorcAdyacencia(1000, 50);
+		g.setGrados();
+		tareas.add( new MulticoreEject(g, "R100050-0",CORRIDAS/2));
+		tareas.add( new MulticoreEject(g, "R100050-1",CORRIDAS/2));
+		
+		g = GrafoGenerator.regularPorcAdyacencia(1000, 75);
+		g.setGrados();
+		tareas.add( new MulticoreEject(g, "R100075-0",CORRIDAS/4));
+		tareas.add( new MulticoreEject(g, "R100075-1",CORRIDAS/4));
+		tareas.add( new MulticoreEject(g, "R100075-2",CORRIDAS/4));
+		tareas.add( new MulticoreEject(g, "R100075-3",CORRIDAS/4));
 		
 		while(!tareas.isEmpty()){
 			while(RUNNING< processors && !tareas.isEmpty()){
--- Desarrollo/src/tp/MulticoreEject.java
+++ Desarrollo/src/tp/MulticoreEject.java
@@ -11,10 +11,11 @@ public class MulticoreEject extends Thread{
 	
 	private String file;
 	private GrafoNDNP g;
-	
-	public MulticoreEject(GrafoNDNP g, String name) {
+	private int corridas;
+	public MulticoreEject(GrafoNDNP g, String name, int corridas) {
 		this.g = g.clone();
 		this.file = name;
+		this.corridas = corridas;
 	}
 	
 	
@@ -26,6 +27,7 @@ public class MulticoreEject extends Thread{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.err.println("Finalizado "+file);
 		Main.finalizo(this);
 	}
 	
@@ -39,7 +41,7 @@ public class MulticoreEject extends Thread{
 		int minSecuecial=0;
 		int minWP=0;
 		long startTime = System.currentTimeMillis();
-		for(int i=0;i<Main.CORRIDAS;i++){
+		for(int i=0;i<corridas;i++){
 			int colores = g.colorearSecuencialAleatorio();
 			if(colores<minSecuecial || i==0){
 				minSecuecial = colores;
@@ -57,22 +59,30 @@ public class MulticoreEject extends Thread{
 				minWP = colores;
 				rminWP= i;
 			}
-			if(i!=0 && i % 100 == 0){
-				System.out.println("("+file+")Tiempo estimado ("+i+"/"+Main.CORRIDAS+"): "+
-			((((System.currentTimeMillis() - startTime)/i)*(Main.CORRIDAS-i))/60000)+"mins");
+			if(i== 100){
+				System.out.println("("+file+")Tiempo estimado ("+i+"/"+corridas+"): "+
+			((((System.currentTimeMillis() - startTime)/i)*(corridas-i))/60000)+"mins");
 			}
 		}
 
+		/* informar en consola resultado
 		System.out.println("Secuencial aleatorio \n\tMinimo: "+minSecuecial+"\n\tCorrida: "+rminSecuecial);
 		System.out.println("Matula \n\tMinimo: "+minMatula+"\n\tCorrida: "+rminMatula);
 		System.out.println("Welsh-Powell \n\tMinimo: "+minWP+"\n\tCorrida: "+rminWP);
+		*/
 		
+		int offset = 0;
+		String[] sp = file.split("-");
+		if(sp.length>1){
+			offset = Integer.valueOf(sp[1])*corridas;
+		}
+		// guarda resultado en archivo
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(file)));
-		writer.write("Secuencial aleatorio \n\tMinimo: "+minSecuecial+"\n\tCorrida: "+rminSecuecial);
+		writer.write("Secuencial aleatorio \n\tMinimo: "+minSecuecial+"\n\tCorrida: "+(rminSecuecial+offset));
 		writer.newLine();
-		writer.write("Matula \n\tMinimo: "+minMatula+"\n\tCorrida: "+rminMatula);
+		writer.write("Matula \n\tMinimo: "+minMatula+"\n\tCorrida: "+(rminMatula+offset));
 		writer.newLine();
-		writer.write("Welsh-Powell \n\tMinimo: "+minWP+"\n\tCorrida: "+rminWP);
+		writer.write("Welsh-Powell \n\tMinimo: "+minWP+"\n\tCorrida: "+(rminWP+offset));
 		writer.close();
 	}
 }
